5666eaa752b952abcf9c86957bf76cad36f5acb8
Xheghun <ysenruf@gmail.com>
2021-05-13 20:35:04 +0400

    create routes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import UserComponent from './components/UserComponent.vue';
+import LessonComponent from './components/LessonComponent.vue';
+
+const routes = [
+  {
+    name: 'lesson',
+    path: '/',
+    component: LessonComponent,
+  },
+  {
+    name: 'user',
+    path: '/user',
+    component: UserComponent,
+  },
+  {
+    name: 'lesson',
+    path: '/lesson',
+    component: LessonComponent,
+  }
+];
+
+
+const router = new VueRouter({mode: 'history', routes: routes});
+
+new Vue(Vue.util.extend({router}, App)).$mount('#app');
